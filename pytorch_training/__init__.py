4e0a222c927dd5fad22e0b0c91c8242f6b9e12d1
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2021-11-08 14:53:52 +0300

    Add new docstrings

--- pytorch_training/__init__.py
+++ pytorch_training/__init__.py
@@ -1,20 +1,24 @@
-"""
-__init__.py
+# pytorch_training/__init__.py
+#
+# Copyright (C) 2021 Дмитрий Кузнецов
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Package pytorch_training.
 
-Copyright (C) 2021 Дмитрий Кузнецов
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+This package contains utilities to train PyTorch models.
 """
 
 from .training import training
+
+__version__: str = '0.1.0'
